f566a05f6530a25b1e1b9720ed8d0c6b9df0b737
Danil Zinurov <zinurov_danil@mail.ru>
2022-09-08 20:54:25 +0300

    Add handle_data.py

--- bot.py
+++ bot.py
@@ -1,15 +1,18 @@
 import config
 import telebot
 
+from hh_parcing import parce_data
+from handle_data import handle_vacancoes
 bot = telebot.TeleBot(config.token)
 
 
 @bot.message_handler(commands=['check'])
 
 def get_vacancies(message):
-    bot.send_message(message.chat.id, 'вакансии hh')
-
-
+    raw_data = parce_data()
+    data = handle_vacancoes(raw_data)
+    for text in data[:5]:
+        bot.send_message(message.chat.id, text)
 
 if __name__ == '__main__':
      bot.infinity_polling()
