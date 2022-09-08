bad6b6fcca9690afb745b8fac354d05583388fba
Danil Zinurov <zinurov_danil@mail.ru>
2022-09-08 20:35:52 +0300

    Add hh_parcing.py

--- bot.py
+++ bot.py
@@ -3,13 +3,13 @@ import telebot
 
 bot = telebot.TeleBot(config.token)
 
-@bot.message_handler(content_types=["text"])
-def repeat_all_messages(message): # Название функции не играет никакой роли
-    bot.send_message(message.chat.id, message.text)
 
-@bot.message_handler(content_types=["text"])
-def foo(message):
-    bot.send_message(message.chat.id, 'Hello')
+@bot.message_handler(commands=['check'])
+
+def get_vacancies(message):
+    bot.send_message(message.chat.id, 'вакансии hh')
+
+
 
 if __name__ == '__main__':
      bot.infinity_polling()
---
+++ hh_parcing.py
@@ -0,0 +1,26 @@
+import requests, time, json
+
+def get_page(page = 0):
+    params = {
+        'text': 'python',  # Текст фильтра. В имени должно быть слово "Аналитик"
+        'area': 16,  # Поиск ощуществляется по вакансиям города Минск
+        'page': page,  # Индекс страницы поиска на HH
+        'per_page': 100,  # Кол-во вакансий на 1 странице
+        'period': 1
+    }
+    req = requests.get('https://api.hh.ru/vacancies', params=params)
+    data = req.content.decode()
+    req.close()
+    return data
+
+def parce_data():
+    lst_Obj = []
+
+    for i in range(11):
+        json_data = get_page(i)
+        jsObj = json.loads(json_data)
+        lst_Obj.extend(jsObj['items'])
+        time.sleep(0.25)
+    return lst_Obj
+
+
